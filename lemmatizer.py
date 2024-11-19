4cc166b4a9c7eb0d3bde3655b83448ea0c6121c7
apresco3 <apresco3@u.rochester.edu>
2024-11-19 14:58:20 -0500

    Finished Part I, Section A and Section B

--- lemmatizer.py
+++ lemmatizer.py
@@ -1,6 +1,13 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+import re
+import string
+from nltk.corpus import stopwords
+
+# Set of stopwords
+STOPWORDS = set(stopwords.words('english'))
 
 lemmatizer = WordNetLemmatizer()
 
@@ -33,3 +40,31 @@ def lemmatize_tweet(tweet):
             #else use the tag to lemmatize the token
             lemmatized_tweet.append(lemmatizer.lemmatize(word, tag))
     return(" ".join(lemmatized_tweet))
+
+def tweet_cleaner(tweet):
+    # Remove links (http/https)
+    tweet = re.sub(r'http\S+|www\S+', '', tweet)
+
+    # Remove punctuation
+    tweet = tweet.translate(str.maketrans('', '', string.punctuation))
+
+    # Remove emojis and non-ASCII characters
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "]+", flags=re.UNICODE)
+    tweet = emoji_pattern.sub(r'', tweet)
+    tweet = re.sub(r'[^\x00-\x7F]+', '', tweet)  # Remove non-ASCII chars
+
+    # Remove non-alphanumeric tokens and words shorter than 3 characters
+    tweet = " ".join([word for word in tweet.split() if word.isalnum() and len(word) > 2])
+
+    # Remove stopwords
+    tweet = " ".join([word for word in tweet.split() if word.lower() not in STOPWORDS])
+
+    # Remove retweet markers and noise terms -> found from part b)
+    tweet = re.sub(r'\b(rt|brt|amp|htt)\b', '', tweet, flags=re.IGNORECASE)
+
+    return tweet
